cdeef3e71711ea6746f8e5a790f8308700ecc569
MarkCodering <mark.chen.sstm@gmail.com>
2023-04-30 09:23:03 +0800

    init deployment commit

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from typing import List
-from fastapi import FastAPI, status
+from fastapi import FastAPI, Request, Form
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware.gzip import GZipMiddleware
-from pydantic import BaseModel
+import openai
+import os
 
 app = FastAPI(title="REST API using FastAPI PostgreSQL Async EndPoints")
 app.add_middleware(
@@ -14,7 +14,26 @@ app.add_middleware(
 )
 app.add_middleware(GZipMiddleware)
 
+# Read the API key from the WSL environment variable
+openai.api_key = os.environ["OPENAI_API_KEY"]
+
 # Create a homepage route
 @app.get("/")
 def index():
     return {"ok": True}
+
+@app.post("/api/ai/docs")
+async def generateDocs(request: Request, aiReq: str = Form(...)):
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt="class Log:\n"+aiReq+"Here's what the above class is doing, explained in a concise way:\n1.",
+        temperature=0,
+        max_tokens=150,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.0,
+        stop=["\"\"\""]
+    )
+    result = response.choices[0].text
+    
+    return {"result": result, "request": aiReq}
